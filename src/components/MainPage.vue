37b41c02002db3f2a55658047052fc25bb45c089
Gary-09 <hlimpiado14@gmail.com>
2024-03-06 19:59:02 +1100

    fixed the video error

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -3,7 +3,8 @@
         <Navbar />
         <div class="content text-center m-5">
             <p class="main">Picture Perfect Memories Awaits!</p>
-            <p class="sub-main">Unlock the Magic of Photography. Immerse Yourself in Our Studio's Captivating Atmosphere.
+            <p class="sub-main">Unlock the Magic of Photography. Immerse Yourself in Our Studio's Captivating
+                Atmosphere.
                 Create Lasting Memories with Professional Portraits and Unforgettable Shots.</p>
         </div>
     </div>
@@ -14,37 +15,52 @@
             <hr>
         </div>
         <br>
-        <div class="d-flex justify-content-center">
-            <div class="card bg-transparent text-bg-dark" style="width: 18rem;">
-                <video class="video" ref="video" controls loop>
-                    <source src="@/assets/images/prod-photo.mp4" type="video/mp4">
-                </video>
-                <div class="card-body">
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
-                        card's content.</p>
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-sm-1 col-md-4 col-lg-4">
+                    <div class="card bg-transparent text-bg-dark" style="width: auto;">
+                        <video class="video" ref="video" autoplay loop muted>
+                            <source src="@/assets/images/prod-photo.mp4" type="video/mp4">
+                        </video>
+                        <div class="card-body">
+                            <h5 class="card-title">Photography</h5>
+                            <p class="card-text">At YourStudio, our Photography services turn moments into timeless art,
+                                capturing the essence of each scene with creativity and precision. Let us frame your
+                                world
+                                and
+                                preserve your memories with beauty and brilliance.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-1 col-md-4 col-lg-4">
+                    <div class="card bg-transparent text-bg-dark" style="width: auto;">
+                        <video class="video" ref="video" autoplay loop muted>
+                            <source src="@/assets/images/prod-vid.mp4" type="video/mp4">
+                        </video>
+                        <div class="card-body">
+                            <h5 class="card-title">Videography</h5>
+                            <p class="card-text">Immerse yourself in cinematic brilliance with YourStudio's Videography
+                                services. We blend cutting-edge technology and captivating narratives to transform your
+                                moments
+                                into visual masterpieces.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-1 col-md-4 col-lg-4">
+                    <div class="card bg-transparent text-bg-dark" style="width: auto;">
+                        <video class="video" ref="video" autoplay loop muted>
+                            <source src="@/assets/images/prod-cin.mp4" type="video/mp4">
+                        </video>
+                        <div class="card-body">
+                            <h5 class="card-title">Cinematography</h5>
+                            <p class="card-text">Elevate your storytelling with YourStudio's Cinematography services.
+                                Our
+                                creative team brings cinematic perfection to every frame, turning ordinary stories into
+                                extraordinary visual experiences.</p>
+                        </div>
+                    </div>
                 </div>
             </div>
-            <div class="card bg-transparent text-bg-dark" style="width: 18rem;">
-                <video class="video" ref="video" controls loop>
-                    <source src="@/assets/images/prod-vid.mp4" type="video/mp4">
-                </video>
-                <div class="card-body">
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
-                        card's content.</p>
-                </div>
-            </div>
-
-            <div class="card bg-transparent text-bg-dark" style="width: 18rem;">
-                <video class="video" ref="video" controls loop>
-                    <source src="@/assets/images/prod-cin.mp4" type="video/mp4">
-                </video>
-                <div class="card-body">
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
-                        card's content.</p>
-                </div>
-            </div>
-
-
         </div>
     </div>
 </template>
